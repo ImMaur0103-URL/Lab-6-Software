a8d0bd079fc5dd12c80ce056302f7ed935a4f601
ImMaur0103 <mau_ranger@hotmail.com>
2024-09-15 22:33:29 -0600

    Setings de pruebas unitarias,y primeras pruebas unitarias

--- APP/tests/test_main.py
+++ APP/tests/test_main.py
@@ -1,5 +1,12 @@
 from unittest.mock import patch
+import json
+import app.main as app
+import app.models as models
 import unittest
+from fastapi.testclient import TestClient
+import asyncio
+
+
 
 class TestProductAPI(unittest.TestCase):
     """def setUp(self):
@@ -39,6 +46,226 @@ class TestProductAPI(unittest.TestCase):
     def test_ejemplo(self):
         pass
         self.assertEqual(1,1)
+
+#2. Lectura de Productos (GET /productos):
+class TEST_2_Lectura_Productos(unittest.TestCase):
+
+    def test_read_products_no_filters(self):
+        # Arrange
+        expected_products_count = 1000
+
+        # Act
+        products = asyncio.run(app.read_products())
+
+        # Assert
+        self.assertEqual(len(products), expected_products_count)
+
+    def test_read_products_name_filter(self):
+        # Arrange
+        expected_products_count = 3
+        expected_products_json = [
+            {
+                "nombre": "Beans - Kidney, Red Dry",
+                "descripcion": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+                "precio": 86.66,
+                "categoria": "Notebook",
+                "inventario": 97,
+                "sku": "BVJXA-SKU-996",
+                "fecha_lanzamiento": "11/30/2024",
+                "imagen_url": "https://ejemplo.com/0p6xf.png",
+                "id": "d1f486f0-6485-4a46-bc38-2657042bfabb"
+            },
+            {
+                "nombre": "Beans - Kidney White",
+                "descripcion": "Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.\n\nEtiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.",
+                "precio": 764.74,
+                "categoria": "Water bottle",
+                "inventario": 44,
+                "sku": "PRLQX-SKU-160",
+                "fecha_lanzamiento": "10/01/2025",
+                "imagen_url": "https://ejemplo.com/kneb3.png",
+                "id": "44e4396e-6b80-4278-97e1-94ee4143b78d"
+            },
+            {
+                "nombre": "Beans - Kidney, Canned",
+                "descripcion": "Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.\n\nNullam porttitor lacus at turpis. Donec posuere metus vitae ipsum. Aliquam non mauris.",
+                "precio": 492.05,
+                "categoria": "Phone case",
+                "inventario": 64,
+                "sku": "ISNPS-SKU-503",
+                "fecha_lanzamiento": "02/17/2025",
+                "imagen_url": "https://ejemplo.com/szhc0.png",
+                "id": "98d2ce3d-a416-4706-8d46-99e52e67bb2e"
+            }
+        ]
+        nombre = "Beans - Kidney"
+        expected_products_model = [models.Product.model_validate(p) for p in expected_products_json]
+
+        # Act
+        products = asyncio.run(app.read_products(nombre=nombre))
+        
+
+        # Assert
+        self.assertEqual(len(products), expected_products_count)
+        self.assertEqual(products, expected_products_model)
+
+    def test_read_products_pricemin_filter(self):
+        # Arrange
+        expected_products_count = 2
+        expected_products_json = [
+            {
+                "nombre": "Cup - 6oz, Foam",
+                "descripcion": "Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+                "precio": 996.76,
+                "categoria": "Water bottle",
+                "inventario": 75,
+                "sku": "CMVEA-SKU-009",
+                "fecha_lanzamiento": "01/23/2025",
+                "imagen_url": "https://ejemplo.com/mp41y.png",
+                "id": "64e0920f-3a95-4980-b047-59c1a206ad78"
+            },
+            {
+                "nombre": "Cocoa Butter",
+                "descripcion": "Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+                "precio": 996.64,
+                "categoria": "Water bottle",
+                "inventario": 99,
+                "sku": "IJKLA-SKU-069",
+                "fecha_lanzamiento": "06/16/2025",
+                "imagen_url": "https://ejemplo.com/qd0mp.png",
+                "id": "aae3b0fd-534e-42db-bbe2-edaf703a9b55"
+            }
+        ]
+        precio = 995.00
+        expected_products_model = [models.Product.model_validate(p) for p in expected_products_json]
+
+        # Act
+        products = asyncio.run(app.read_products(precio_min=precio))
+        
+
+        # Assert
+        self.assertEqual(len(products), expected_products_count)
+        self.assertEqual(products, expected_products_model)
+
+    def test_read_products_pricemax_filter(self):
+        # Arrange
+        expected_products_count = 6
+        expected_products_json = [
+            {
+                "nombre": "Energy - Boo - Koo",
+                "descripcion": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+                "precio": 14.48,
+                "categoria": "Phone case",
+                "inventario": 30,
+                "sku": "MKRMO-SKU-872",
+                "fecha_lanzamiento": "03/12/2025",
+                "imagen_url": "https://ejemplo.com/p8lk4.png",
+                "id": "d6f69784-5e63-43e1-9fbe-3bfd00bc1cf5"
+            },
+            {
+                "nombre": "Wine - Rioja Campo Viejo",
+                "descripcion": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.\n\nCurabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.",
+                "precio": 11.78,
+                "categoria": "Phone case",
+                "inventario": 17,
+                "sku": "MZYDI-SKU-947",
+                "fecha_lanzamiento": "11/01/2025",
+                "imagen_url": "https://ejemplo.com/7530m.png",
+                "id": "f98b1e3a-26f6-44f1-8e3b-e26475fb83b0"
+            },
+            {
+                "nombre": "Dc - Sakura Fu",
+                "descripcion": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.\n\nMorbi porttitor lorem id ligula. Suspendisse ornare consequat lectus. In est risus, auctor sed, tristique in, tempus sit amet, sem.",
+                "precio": 12.12,
+                "categoria": "Phone case",
+                "inventario": 10,
+                "sku": "HABCT-SKU-132",
+                "fecha_lanzamiento": "01/25/2025",
+                "imagen_url": "https://ejemplo.com/xu54e.png",
+                "id": "9e6be5b2-69f7-4453-92fa-6c2724467980"
+            },
+            {
+                "nombre": "Sauce - Demi Glace",
+                "descripcion": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin risus. Praesent lectus.",
+                "precio": 14.3,
+                "categoria": "Notebook",
+                "inventario": 70,
+                "sku": "VJINO-SKU-009",
+                "fecha_lanzamiento": "10/28/2025",
+                "imagen_url": "https://ejemplo.com/ryc54.png",
+                "id": "5b243420-dcd4-4fe0-9500-ebcdc2bb0d61"
+            },
+            {
+                "nombre": "Chocolate - Chips Compound",
+                "descripcion": "Fusce consequat. Nulla nisl. Nunc nisl.",
+                "precio": 12.19,
+                "categoria": "Phone case",
+                "inventario": 1,
+                "sku": "PYKOM-SKU-883",
+                "fecha_lanzamiento": "04/24/2025",
+                "imagen_url": "https://ejemplo.com/829ln.png",
+                "id": "817e4cdb-c009-4368-8524-068f236eb447"
+            },
+            {
+                "nombre": "Sponge Cake Mix - Vanilla",
+                "descripcion": "Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.",
+                "precio": 12.71,
+                "categoria": "Phone case",
+                "inventario": 57,
+                "sku": "TAKSG-SKU-195",
+                "fecha_lanzamiento": "12/05/2024",
+                "imagen_url": "https://ejemplo.com/a81nt.png",
+                "id": "e55071b3-3a6a-4738-b6a4-9a4507ca50bf"
+            }
+        ]
+        precio = 15.00
+        expected_products_model = [models.Product.model_validate(p) for p in expected_products_json]
+
+        # Act
+        products = asyncio.run(app.read_products(precio_max=precio))
+        
+
+        # Assert
+        self.assertEqual(len(products), expected_products_count)
+        self.assertEqual(products, expected_products_model)
+
+    def test_read_products_category_filter(self):
+        # Arrange
+        expected_products_count = 1
+        expected_products_json = [
+            {
+                "nombre": "Taro Leaves",
+                "descripcion": "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.",
+                "precio": 608.24,
+                "categoria": "Tarot",
+                "inventario": 35,
+                "sku": "ILQNG-SKU-428",
+                "fecha_lanzamiento": "09/07/2025",
+                "imagen_url": "https://ejemplo.com/76xbk.png",
+                "id": "0e091c7f-9008-4afa-94cd-9afe8bbf6e4f"
+            }
+        ]
+        categoria = "Tarot"
+        expected_products_model = [models.Product.model_validate(p) for p in expected_products_json]
+
+        # Act
+        products = asyncio.run(app.read_products(categoria=categoria))
+        
+
+        # Assert
+        self.assertEqual(len(products), expected_products_count)
+        self.assertEqual(products, expected_products_model)
+
+#4. Actualización de Producto (PUT /productos/{id}):
+class TEST_3_Lectura_Producto_Específico(unittest.TestCase):
+    def setUp(self):
+        self.id = "cddf966f-2a1d-47fd-871a-bad6c7e408f9"
+        self.original_Product = {"id":"cddf966f-2a1d-47fd-871a-bad6c7e408f9","nombre":"Pasta - Fusili, Dry","descripcion":"Pasta Penne","precio":284.54,"categoria":"Pasta","inventario":27,"sku":"UFWQP-SKU-891","fecha_lanzamiento":"05/23/2025","imagen_url":"https://ejemplo.com/at58h.png"}
+        self.update_Product = {"id":"cddf966f-2a1d-47fd-871a-bad6c7e408f9","nombre":"Pasta - Fusili, Dry","descripcion":"Pasta Penne de secado rapido","precio":5.50,"categoria":"Pasta","inventario":100,"sku":"PASTA-SKU-891","fecha_lanzamiento":"05/23/2000","imagen_url":"https://ejemplo.com/at58h.png"}
+        
+
+    def test_Product_update(sefl):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
